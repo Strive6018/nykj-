46758be1f0648965b1bf2b81a555f17033b5a517
dreamisfarfly <dreamisfarfly@gmail.com>
2019-09-23 15:23:21 +0800

    测试仓库

--- Lib/Lib/Action/Api/Repository/ApiModel.class.php
+++ Lib/Lib/Action/Api/Repository/ApiModel.class.php
@@ -1,6 +1,5 @@
 <?php
 require_once APP_PATH.'./Common/Api/Consts.php';
-
 class ApiModel extends RelationModel
 {
     protected function _initialize()
@@ -19,27 +18,27 @@ class ApiModel extends RelationModel
     /**
      * 通用分页
      */
-    public function page($where,$model,$params,$model_viwe)
-    {
-        $data = [];
-        $data['totalCount'] = $model->ff_select_counts($where);
-        $data['totalPages'] = ceil($data['totalCount'] /$params['limit']);
-        $data['page'] = $params['page_id'];
-        if($data['page'] > $data['totalPages']){
-            $data['items'] = [];
-        }else{
-            $data['items'] = $model->ff_select_pages($params,$where,$model_viwe);
-        }
-        if($params['page']==1){
-            $data['isFirst'] = true;
-        }else{
-            $data['isFirst'] = false;
-        }
-        if($data['totalPages'] == $params['page']){
-            $data['isLast'] = true;
-        }else{
-            $data['isLast'] = false;
-        }
-        return $data;
-    }
+//    public function page($where,$model,$params,$model_viwe)
+//    {
+//        $data = [];
+//        $data['totalCount'] = $model->ff_select_counts($where);
+//        $data['totalPages'] = ceil($data['totalCount'] /$params['limit']);
+//        $data['page'] = $params['page_id'];
+//        if($data['page'] > $data['totalPages']){
+//            $data['items'] = [];
+//        }else{
+//            $data['items'] = $model->ff_select_pages($params,$where,$model_viwe);
+//        }
+//        if($params['page']==1){
+//            $data['isFirst'] = true;
+//        }else{
+//            $data['isFirst'] = false;
+//        }
+//        if($data['totalPages'] == $params['page']){
+//            $data['isLast'] = true;
+//        }else{
+//            $data['isLast'] = false;
+//        }
+//        return $data;
+//    }
 }
