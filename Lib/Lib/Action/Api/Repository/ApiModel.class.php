d0215bd3bf74e0d8ceb2fc2b9041881e2f49f7a4
dreamisfarfly <dreamisfarfly@gmail.com>
2019-09-26 15:51:32 +0800

    修改公共Model

--- Lib/Lib/Action/Api/Repository/ApiModel.class.php
+++ Lib/Lib/Action/Api/Repository/ApiModel.class.php
@@ -15,30 +15,4 @@ class ApiModel extends RelationModel
         if(is_array($where)) $where = array_shift($where);
         return'Strive_'.md5("$where (string)$field)");
     }
-    /**
-     * 通用分页
-     */
-//    public function page($where,$model,$params,$model_viwe)
-//    {
-//        $data = [];
-//        $data['totalCount'] = $model->ff_select_counts($where);
-//        $data['totalPages'] = ceil($data['totalCount'] /$params['limit']);
-//        $data['page'] = $params['page_id'];
-//        if($data['page'] > $data['totalPages']){
-//            $data['items'] = [];
-//        }else{
-//            $data['items'] = $model->ff_select_pages($params,$where,$model_viwe);
-//        }
-//        if($params['page']==1){
-//            $data['isFirst'] = true;
-//        }else{
-//            $data['isFirst'] = false;
-//        }
-//        if($data['totalPages'] == $params['page']){
-//            $data['isLast'] = true;
-//        }else{
-//            $data['isLast'] = false;
-//        }
-//        return $data;
-//    }
 }
