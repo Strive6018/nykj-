e90faaaddc243d75c8834e2168d7aba7f0070e67
dreamisfarfly <dreamisfarfly@gmail.com>
2019-09-26 15:55:56 +0800

    修改金币明细接口（使用公共分页方法）

--- Lib/Lib/Action/Api/CoinListAction.class.php
+++ Lib/Lib/Action/Api/CoinListAction.class.php
@@ -22,15 +22,13 @@ class CoinListAction extends ApiAction
      * 逻辑层
      */
     private function  handle(){
-        $params =  implode(',',[
-            'user_id as id',
-            'createtime',
-            'type',
-            'coin',
-            'action',
-            'description'
-        ]);
-        $this->data=$this->model->field($params)->where(array('user_id'=>$this->user_id))->select();
+        $params['page_id'] = $this->request['page'];
+        $params['page_is'] = true;
+        $params['limit'] = $this->request['size'];
+        $params['page_p'] = $this->request['page'];
+        $params['page'] = $this->request['page'];
+        $where = array('user_id '=>$this->user_id);
+        $this->data = $this->page($where,$this->model,$params);
     }
 
     private function response(){
@@ -41,6 +39,33 @@ class CoinListAction extends ApiAction
         $this->validate();
         $this->handle();
         $this->response();
+    }
+
+    /**
+     * 通用分页
+     */
+    private function page($where,$model,$params,$model_viwe)
+    {
+        $data = [];
+        $data['totalCount'] = $model->ff_select_counts($where);
+        $data['totalPages'] = ceil($data['totalCount'] /$params['limit']);
+        $data['page'] = $params['page_id'];
+        if($data['page'] > $data['totalPages']){
+            $data['items'] = [];
+        }else{
+            $data['items'] = $model->ff_select_pages($params,$where,$model_viwe);
+        }
+        if($params['page']==1){
+            $data['isFirst'] = true;
+        }else{
+            $data['isFirst'] = false;
+        }
+        if($data['totalPages'] == $params['page']){
+            $data['isLast'] = true;
+        }else{
+            $data['isLast'] = false;
+        }
+        return $data;
     }
 
 }
